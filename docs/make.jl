4374670bab83ea4b787b925fd8a197aee8498be2
Arno Strouwen <arno.strouwen@telenet.be>
2023-09-24 23:55:33 +0200

    Documenter 1.0 upgrade

--- docs/make.jl
+++ docs/make.jl
@@ -6,17 +6,9 @@ cp(joinpath(dir, "README.md"), joinpath(dir, "docs", "src", "index.md"), force =
 makedocs(sitename = "SciML Style Guide for Julia",
          authors = "Chris Rackauckas",
          modules = Module[],
-         clean = true, doctest = false,
-         strict = [
-             :doctest,
-             :linkcheck,
-             :parse_error,
-             :example_block,
-             # Other available options are
-             # :autodocs_block, :cross_references, :docs_block, :eval_block, :example_block, :footnote, :meta_block, :missing_docs, :setup_block
-         ],
-         format = Documenter.HTML(analytics = "UA-90474609-3",
-                                  assets = ["assets/favicon.ico"],
+         clean = true, doctest = false, linkcheck = true,
+         warnonly = [:cross_references],
+         format = Documenter.HTML(assets = ["assets/favicon.ico"],
                                   canonical = "https://docs.sciml.ai/SciMLStyle/stable/"),
          pages = [
              "SciML Style Guide for Julia" => "index.md",
