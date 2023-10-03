f52d63bcd5b18c257da4021ff2a8859bcb73c295
Arno Strouwen <arno.strouwen@telenet.be>
2023-10-03 11:25:45 +0200

    ignore exponentiation link

--- docs/make.jl
+++ docs/make.jl
@@ -7,6 +7,7 @@ makedocs(sitename = "SciML Style Guide for Julia",
          authors = "Chris Rackauckas",
          modules = Module[],
          clean = true, doctest = false, linkcheck = true,
+         linkcheck_ignore = ["https://docs.julialang.org/en/v1/base/math/#Base.:^-Tuple{Number,%20Number}"],
          format = Documenter.HTML(assets = ["assets/favicon.ico"],
                                   canonical = "https://docs.sciml.ai/SciMLStyle/stable/"),
          pages = [
