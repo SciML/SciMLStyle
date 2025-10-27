513ffa341ca9d378f12560c62040b24e82b18223
Stefan Pinnow <Mo-Gul@gmx.net>
2025-10-27 18:49:56 +0100

    add some more `linkcheck_ignore` entries
    which caused errors

--- docs/make.jl
+++ docs/make.jl
@@ -16,7 +16,11 @@ makedocs(sitename = "SciML Style Guide for Julia",
          authors = "Chris Rackauckas",
          modules = Module[],
          clean = true, doctest = false, linkcheck = true,
-         linkcheck_ignore = ["https://docs.julialang.org/en/v1/base/math/#Base.:^-Tuple{Number,%20Number}"],
+         linkcheck_ignore = [
+             "https://docs.julialang.org/en/v1/base/math/#Base.:^-Tuple{Number,%20Number}",
+             "https://github.com/SciML/OrdinaryDiffEq.jl/blob/v6.10.0/test/runtests.jl",
+             "https://documenter.juliadocs.org/stable/man/doctests/",
+         ],
          format = Documenter.HTML(assets = ["assets/favicon.ico"],
                                   canonical = "https://docs.sciml.ai/SciMLStyle/stable/"),
          pages = [
